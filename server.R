27f3e4a22cadd26e9e2d46d038745c5335058117
Sarah Burge <sarah.w.burge@gmail.com>
2016-03-15 10:03:15 +0000

    Added validation to limit user query to 20 genes.

--- server.R
+++ server.R
@@ -13,15 +13,22 @@ library(reshape2)
 # TS_GFP.geneLogFoldData <- readRDS("data/TS_GFP.geneLogFoldData.rds")
 all.geneLogFoldData<- readRDS("data/all.geneLogFoldData.rds")           
 
-shinyServer(function(input, output) { 
+shinyServer(function(input, output) {
+  
   output$value <- renderPrint({ input$cellType })
   output$text1 <-renderText({paste("Gene(s) chosen: ", input$geneSymbol)})
   output$plot1 <- renderPlot ({
+    
     gS<-unlist(strsplit(input$geneSymbol, split=" "))
-   cell<-input$cellType
-   cell<-gsub("1","all",cell)
-   cell<-gsub("2","Rs26",cell)
-   cell<-gsub("3","TS_GFP",cell)
+    
+    validate(
+      need(length(gS)<20, "You have too many genes. Please use less than 20")
+    )
+    
+    cell<-input$cellType
+    cell<-gsub("1","all",cell)
+    cell<-gsub("2","Rs26",cell)
+    cell<-gsub("3","TS_GFP",cell)
       data<-all.geneLogFoldData[all.geneLogFoldData$geneID %in% gS&all.geneLogFoldData$cellType %in% cell,]
       ggplot(data,
              aes(x=Day,y=value,color=geneID,group=interaction(geneID,cellType)),
